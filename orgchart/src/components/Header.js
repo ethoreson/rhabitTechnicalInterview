1980616f51fa8002a39e2fb28db76b47b53d05ff
Emilie Thoreson <thoresonemilie@gmail.com>
2018-03-07 12:23:29 -0800

    basic json display and minimal css

--- orgchart/src/components/Header.js
+++ orgchart/src/components/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 class Header extends Component {
 	render() {
 		return (
 			<div className="header">
-				<h1>rhabit Organizational Chart</h1>
+				<img className="logo" src={require('./rhabit_logo.png')} />
+				<h1 className="title">Organizational Chart</h1>
 			</div>
 		)
 	}
